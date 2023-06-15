b331b2dabfaf77c3dfdfd6a806abcaf8eab0aec1
Noel Guillen <noelblas274@gmail.com>
2023-06-15 15:12:02 -0400

    added screen loader and image slider timeout

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,36 @@ import './App.css'
 import Header from './Components/Header/Header'
 import Homepage from './Pages/Homepage/Homepage'
 import Footer from './Components/Footer/Footer'
+import { Dna } from  'react-loader-spinner'
+import { useState, useEffect } from 'react'
 
 function App() {
   const apiKey = import.meta.env.VITE_API_KEY;
   const baseUrl = import.meta.env.VITE_BASE_URL;
+  const [loading, setLoading] = useState(true);
 
+   useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  }, []);
 
   return (
     <>
+   {
+    loading ?
+  <div className="loader">
+  <Dna  height="300"
+  width="200"/>
+</div>
+
+  :
+  <>
     <Header />
     <Homepage apiKey={apiKey} baseUrl={baseUrl}/>
     <Footer />
+  </>
+   }
     </>
   )
 }
--- src/Components/Slider/Slider.jsx
+++ src/Components/Slider/Slider.jsx
@@ -1,68 +1,74 @@
-import {useEffect, useState} from 'react';
-import "./Slider.css";
+import { useEffect, useState } from 'react';
+import './Slider.css';
 import axios from 'axios';
 import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from 'react-icons/md';
 import StarRatings from 'react-star-ratings';
 
-function Slider({apiKey, baseUrl}) {
-    const [upcomingMovies, setUpComingMovies] = useState([]);
-    const imageBaseUrl = import.meta.env.VITE_IMAGE_BASE_URL;
-    const [index, setIndex] = useState(0);
+function Slider({ apiKey, baseUrl }) {
+  const [upcomingMovies, setUpComingMovies] = useState([]);
+  const imageBaseUrl = import.meta.env.VITE_IMAGE_BASE_URL;
+  const [index, setIndex] = useState(0);
 
-    useEffect(
-        ()=>{
-            axios.get(`${baseUrl}upcoming?api_key=${apiKey}`)
-            .then((result) =>{
-                setUpComingMovies(result.data.results)
-            })
-            .catch((error)=> console.log("error message", error))
-        },[]);
+  useEffect(() => {
+    axios
+      .get(`${baseUrl}upcoming?api_key=${apiKey}`)
+      .then((result) => {
+        setUpComingMovies(result.data.results);
+      })
+      .catch((error) => console.log('error message', error));
+  }, []);
 
-   const sliderStyle = {
-        backgroundImage: `url("${imageBaseUrl}${upcomingMovies[index]?.backdrop_path}")`,
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-        backgroundRepeat: 'no-repeat',
-        height: '60vh',
-        position: 'relative'
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setIndex((prevIndex) => (prevIndex + 1) % upcomingMovies.length);
+    }, 15000);
+
+    return () => {
+      clearInterval(interval);
     };
+  }, [upcomingMovies]);
 
-    const handleRight = ()=>{
-          if(index === upcomingMovies.length - 1){
-            setIndex(0);
-            return; //use to break out the loop
-        }
-        setIndex(prevState => prevState + 1);
-    };
+  const sliderStyle = {
+    backgroundImage: `url("${imageBaseUrl}${upcomingMovies[index]?.backdrop_path}")`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    backgroundRepeat: 'no-repeat',
+    height: '60vh',
+    position: 'relative',
+    transition: 'background-image 0.1s ease-in-out', // Added CSS transition
+  };
 
-    const handleLeft = ()=>{
-        if(index === 0){
-            setIndex(upcomingMovies.length - 1);
-            return; //use to break out the loop
-        }
-        setIndex(prevState=> prevState - 1);
-    };
-     //* if true then render the JSX else show nothing line 56
+  const handleRight = () => {
+    setIndex((prevIndex) => (prevIndex + 1) % upcomingMovies.length);
+  };
+
+  const handleLeft = () => {
+    setIndex((prevIndex) =>
+      prevIndex === 0 ? upcomingMovies.length - 1 : prevIndex - 1
+    );
+  };
+
   return (
     <div className='slider-container' style={sliderStyle}>
-        <div className="slider-overlay">
-            <MdKeyboardArrowLeft className='left-arrow' onClick={handleLeft}/>
-            <MdKeyboardArrowRight className='right-arrow' onClick={handleRight}/>
-            <div className="slider-info">
-                <h1>{upcomingMovies[index]?.title}</h1>
-                <p>{upcomingMovies[index]?.overview}</p>
-                <p>Release Date: {upcomingMovies[index]?.release_date}</p>
-                {upcomingMovies[index] &&
-               <StarRatings
-                rating={upcomingMovies[index]?.vote_average / 2}
-                 starRatedColor={"#FDCC0D"}
-                 starDimension="15px"
-                 starSpacing='1px'/>
-                 }
-            </div>
+      <div className='slider-overlay'>
+        <MdKeyboardArrowLeft className='left-arrow' onClick={handleLeft} />
+        <MdKeyboardArrowRight className='right-arrow' onClick={handleRight} />
+        <div className='slider-info'>
+          <h1>{upcomingMovies[index]?.title}</h1>
+          <p>{upcomingMovies[index]?.overview}</p>
+          <p>Release Date: {upcomingMovies[index]?.release_date}</p>
+          {upcomingMovies[index] && (
+            <StarRatings
+              rating={upcomingMovies[index]?.vote_average / 2}
+              starRatedColor={'#FDCC0D'}
+              starDimension='15px'
+              starSpacing='1px'
+            />
+          )}
         </div>
+      </div>
     </div>
-  )
-};
+  );
+}
 
-export default Slider
+export default Slider;
