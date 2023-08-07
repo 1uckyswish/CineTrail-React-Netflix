0ca0b05900dafde055917aef4c92571ccc17093d
Noel Guillen <noelblas274@gmail.com>
2023-08-07 18:47:48 -0400

    index added 200

--- src/Context/UserContext.jsx
+++ src/Context/UserContext.jsx
@@ -2,8 +2,8 @@ import {useState,useEffect, createContext} from 'react'
 export const UserContext= createContext({})
 
 export default function UserContextProvider(props) {
-    const [user, setUser]=useState('')
-    const [token,setToken]=useState('')
+     const [user, setUser]=useState('')
+     const [token,setToken]=useState('')
 
 useEffect(()=>{
     const user = localStorage.getItem('userInfo')
